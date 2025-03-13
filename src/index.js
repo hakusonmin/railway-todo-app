075942ed424274dde09c03acfd82f07fa6ab96b5
hakusonmin <fujitakosei7@gmail.com>
2025-03-14 02:32:04 +0900

    バージョンアップによる修正

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { CookiesProvider } from "react-cookie";
 import { Provider } from "react-redux";
-import { store } from "./store"
+import { store } from "./store";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
-  </Provider>,
-  document.getElementById("root")
+createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <CookiesProvider>
+            <App />
+        </CookiesProvider>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
